ffb7e0309912b805a55048ebcab71487c744f60f
ebielle <eryblongo@gmail.com>
2024-04-17 17:44:20 +0200

    lala

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,5 @@ public class Main {
     public static void main(String[] args) {
         Smartphone smartphone1 = new Smartphone("Smartphone", "Apple", "Iphone12", " ", "7''", "128GB", "899€", "999€", "8274517193");
         System.out.println(smartphone1);
-
     }
-
 }
