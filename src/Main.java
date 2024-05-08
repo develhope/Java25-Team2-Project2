3c777cff58beb8b83340f5e3dcecf153afdc7548
ebielle <eryblongo@gmail.com>
2024-05-08 15:51:01 +0200

    booo

--- src/Main.java
+++ src/Main.java
@@ -4,38 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-            Menu menu = new Menu();
-            Magazzino tipiDiProdotto = new Magazzino();
-            //Carrello carrello = new Carrello();
-            Scanner scanner = new Scanner(System.in);
-            int scelta;
+        Scanner scanner = new Scanner(System.in);
 
-        do {
+        System.out.println("Selezionare operazione:");
+        System.out.println("1 - Accesso al magazzino");
+        int scelta = scanner.nextInt();
 
-            System.out.println("Selezionare operazione:");
-            System.out.println("0 - Uscita");
-            System.out.println("1 - Accesso al magazzino");
-            System.out.println("2 - Prodotti disponibili");
-            System.out.println("3 - Accesso al carrello");
-            scelta = scanner.nextInt();
+        switch (scelta) {
+            case 1:
+                System.out.println("Accesso al magazzino" + "\n");
+                Magazzino artDisp = new Magazzino();
+                artDisp.disponibili();
+                break;
+            case 2:
+                System.out.println("Accesso al carrello");
+                break;
+            case 3:
+                System.out.println("Fai qualcosa");
+                break;
+            case 4:
+                System.out.println("Fai un'altra cosa");
+                break;
+            default:
+                System.out.println("Scelta non valida");
+        }
 
-            switch (scelta) {
-                case 0:
-                    System.out.println("Uscita in corso");
-                    break;
-                case 1:
-                    System.out.println(tipiDiProdotto.disponibili());
-                    break;
-                case 2:
-                    System.out.println("ricerca per modello");
-                    break;
-                case 3:
-                    menu.creazioneCarrello();
-                    break;
-                default:
-                    System.out.println("Scelta non valida");
-            }
-        } while (scelta != 0);
         scanner.close();
     }
 }
