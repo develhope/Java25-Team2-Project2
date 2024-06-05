335f3e5b4cefc867edcb8a7cd0d4c43a072be472
ebielle <eryblongo@gmail.com>
2024-06-05 10:52:47 +0200

    code review 05/06

--- src/Carrello.java
+++ src/Carrello.java
@@ -1,21 +1,26 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//todo togliere scanner.
 public class Carrello {
 
     public ArrayList<Prodotti> articoli;
     Scanner scanner;
 
+    //todo inserire come field totale carrello, nome carrello, lista prodotti.
     public Carrello() {
         articoli = new ArrayList<>();
         scanner = new Scanner(System.in);
     }
 
+    //todo ogni volta che aggiungiamo si somma l'articolo al totale. un field come contatore
     public ArrayList<Prodotti> aggiungiProdotto(Prodotti articolo) {
         articoli.add(articolo);
+        //todo sommare articolo e aggiornare il totale.
         return articoli;
     }
 
+    //todo sottrarre il costo anche.
     public ArrayList<Prodotti> rimuoviProdotto(Prodotti articolo) {
         articoli.remove(articolo);
         return articoli;
@@ -42,6 +47,7 @@ public class Carrello {
         return result / articoli.size();
     }
 
+    //todo eliminare
     public String finalizzaOperazioneVendita() {
         if (articoli.isEmpty()) {
             return "Il carrello è vuoto.";
